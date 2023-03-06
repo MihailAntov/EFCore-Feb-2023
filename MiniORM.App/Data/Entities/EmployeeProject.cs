42bfa3b3eab4366d31b9132bf53240b5d97dbdfb
MihailAntov <114488260+MihailAntov@users.noreply.github.com>
2023-03-06 17:53:20 +0200

    exercise added - orm fundamentals

---
+++ MiniORM.App/Data/Entities/EmployeeProject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniORM.App.Data.Entities
+{
+    public class EmployeeProject
+    {
+        [Key]
+        [ForeignKey(nameof(Employee))]
+        public int EmployeeId { get; set; }
+        [Key]
+        [ForeignKey(nameof(Project))]
+        public int ProjectId { get; set; }
+        public Employee Employee { get; set; }
+        public Project Project { get; set; }
+    }
+}
---
+++ MiniORM.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using MiniORM.App.Data.Entities;
+using MiniORM.App.Data;
+
+var connectionString = "Server=.;Database=MiniORM;Integrated Security=true;Encrypt=False";
+
+var context = new SoftUniDbContext(connectionString);
+
+context.Employees.Add(new Employee
+{
+    FirstName = "Test",
+    LastName = "Test Insert",
+    DepartmentId = context.Departments.First().Id,
+    IsEmployed = true
+});
+
+var employee = context.Employees.Last();
+employee.FirstName = "Modified";
+context.SaveChanges();
